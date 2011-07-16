be81c5cce2a7cbbf316f1a57f6ae1c6b2faa7090
Kevin Deldycke <kevin@deldycke.com>
2011-07-16 17:39:12 +0200

    Use a template to display our first page.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import cherrypy
 class Root():
 
     @cherrypy.expose
+    @cherrypy.tools.mako(filename="base.mako")
     def default(self, *args, **kwargs):
-        return "Hello World !"
+        return
 
